c28f74b6acfb9b466f267a4bc6a7f9e93a5a4976
BRdev-by <yagizbaran40@gmail.com>
2024-09-18 00:40:23 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- Asansörün konumları
+Config.ElevatorLocations = {
+    { x = 440.0, y = -984.0, z = 30.0 }, -- Zemin kat
+    { x = 440.0, y = -984.0, z = 50.0 }, -- Üst kat
+}
+
+-- Asansör tuşuna basmak için gerekli mesafe
+Config.ElevatorInteractionDistance = 2.0
